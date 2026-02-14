244d77add81b22428ee74b5c275d8e3114fa30a8
dxnea <dineshdini909@gmail.com>
2026-02-14 05:19:25 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,24 +9,39 @@ terraform {
   backend "s3" {
     bucket = "cluster-003"
     key    = "ajs/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-southeast-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-1"
 }
 
 locals {
-  region = "us-east-1"
+  region = "ap-southeast-1"
   name   = "vgs_cluster"
 
   vpc_cidr = "10.123.0.0/16"
 
-  azs              = ["us-east-1a", "us-east-1b"]
-  public_subnets   = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets  = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets    = ["10.123.5.0/24", "10.123.6.0/24"]
+  azs = [
+    "ap-southeast-1a",
+    "ap-southeast-1b"
+  ]
+
+  public_subnets = [
+    "10.123.1.0/24",
+    "10.123.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.123.3.0/24",
+    "10.123.4.0/24"
+  ]
+
+  intra_subnets = [
+    "10.123.5.0/24",
+    "10.123.6.0/24"
+  ]
 
   tags = {
     Example = "vgs_cluster"
