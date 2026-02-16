efac9bf2135170ab00f636786e36a024a39ca6ba
dxnea <dineshdini909@gmail.com>
2026-02-15 22:58:57 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,11 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
-
+ # Adding Backend as S3 for Remote State Storage
   backend "s3" {
     bucket = "cluster-003-dinesh"
     key    = "arn:aws:s3:::cluster-003-dinesh"
-    region = "us-east-1"
+    region = "us-east-1"   
   }
 }
 
@@ -19,32 +19,13 @@ provider "aws" {
 
 locals {
   region = "us-east-1"
-  name   = "vgs_cluster"
-
+  name = "vgs_cluster"
   vpc_cidr = "10.123.0.0/16"
-
-  azs = [
-    "us-east-1a",
-    "us-east-1b"
-  ]
-
-  public_subnets = [
-    "10.123.1.0/24",
-    "10.123.2.0/24"
-  ]
-
-  private_subnets = [
-    "10.123.3.0/24",
-    "10.123.4.0/24"
-  ]
-
-  intra_subnets = [
-    "10.123.5.0/24",
-    "10.123.6.0/24"
-  ]
-
+  azs      = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
   tags = {
-    Example = "vgs_cluster"
+    Example = local.name
   }
 }
-
